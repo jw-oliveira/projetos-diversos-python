6889fd28e47db8c7f5668b709b1ffa274463977d
Jorge Willian de Oliveira <jorge_willian@outlook.com>
2022-11-03 17:30:43 -0300

    add-api

---
+++ API_Python/app.py
@@ -0,0 +1,72 @@
+# API - Ligar para disponibilizar recursos e/ou funcionalidades
+# 1. Objetivo - Criar um API que disponibiliza a consulta, criação,
+# edição e exclusão de livros
+# 2. URL base - localhost
+# 3. Endpoints - 
+#     -localhost/livros (GET)
+#     -localhost/livros/id (GET)
+#     -localhost/livros/id (PUT)
+#     -localhost/livros/id (DELETE)
+# 4. Quais recursos - Livros
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'O Senhor dos Anéis -  A Sociedade do Anel',
+        'autor': 'J.R.R Tolkien',
+    },
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e Pedra Filosofal',
+        'autor': 'J.K. Howling'
+    },
+    {
+        'id': 3,
+        'titulo': 'Habitos Atômicos',
+        'autor': 'James Clear',
+    },
+]
+
+# Consultar (todos)
+@app.route('/livros', methods=['GET'])
+def obterLivros():
+    return jsonify(livros)
+
+# Consultar por ID
+@app.route('/livros/<int:id>', methods=['GET'])
+def obterLivroPorID(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+# Editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editarLivroPorID(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+# Criar
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
+# excluir
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+        return jsonify(livros)
+
+app.run(port=5000,host='localhost',debug=True)
