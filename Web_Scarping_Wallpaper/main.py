9477863452f721676ea63b1333386c32ad9ce13f
Jorge Willian de Oliveira <jorge_willian@outlook.com>
2022-12-02 20:59:17 -0300

    add-export-to-file

--- Web_Scarping_Wallpaper/main.py
+++ Web_Scarping_Wallpaper/main.py
@@ -1,13 +1,10 @@
 import requests
-from PIL import Image
 from bs4 import BeautifulSoup as bs
-
 
 individual_pages = []
 
 
-def find_page():
-    url = 'https://www.hdcarwallpapers.com/latest_wallpapers?page=2'
+def find_page(url):
     r = requests.get(url)
     soup = bs(r.content, features='html.parser')
     image_names = soup.find_all('em1')
@@ -17,20 +14,24 @@ def find_page():
         link = str(image_names[cont]).replace('<em1>', '').lower()
         link = link.replace('</em1>', '')
         link = link.replace(' ', '_')
+        link = link.replace('.', '_')
+        link = link.replace('-', '_')
         link = (f'https://www.hdcarwallpapers.com/walls/{link}-HD.jpg')
         
         individual_pages.append(link)
 
 
-def downloader(pages):    
-    url = pages[5]
-    img = Image.open(requests.get(url, stream=True).raw)
-    img.save('picture.jpg')
-     
-   
-find_page()
-downloader(individual_pages)
+def export_to_txt(list_urls):
+    with open(r'/Web_Scarping_Wallpaper/download_list.txt', 'w') as fp:
+        for link in list_urls:
+            # write each item on a new line
+            fp.write("%s\n" % link)
 
+     
+url = str(input('Digite a URL: '))
+
+find_page(url)
+export_to_txt(individual_pages)
 
 
     
