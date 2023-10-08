0f26d7dbff0c906f75241c1e8ed5dd02f5d37b41
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2023-10-08 18:15:45 -0300

    add multi page scanning
    Adds the possibility of scanning multiple pages, entering the number of the first and last page to be searched.

--- Web_Scarping_Wallpaper/web_scraping_wallpaper.py
+++ Web_Scarping_Wallpaper/web_scraping_wallpaper.py
@@ -4,46 +4,32 @@ from bs4 import BeautifulSoup as bs
 individual_pages = []
 
 
-def find_page(url):
-    r = requests.get(url)
-    soup = bs(r.content, features='html.parser')
-    image_names = soup.find_all('em1')
+def create_file(first_page, last_page):
+    for page in range(first_page, last_page + 1):
+        r = requests.get(f'https://www.hdcarwallpapers.com/latest_wallpapers?page={page}')
+        soup = bs(r.content, features='html.parser')
+        image_names = soup.find_all('em1')
 
+        for cont in range(0, len(image_names)):
+            link = str(image_names[cont]).replace('<em1>', '').lower()
+            link = link.replace('</em1>', '')
+            link = link.replace(' ', '_')
+            link = link.replace('.', '_')
+            link = link.replace('-', '_')
+            link = f'https://www.hdcarwallpapers.com/walls/{link}-HD.jpg'
 
-    for cont in range(0, len(image_names)):
-        link = str(image_names[cont]).replace('<em1>', '').lower()
-        link = link.replace('</em1>', '')
-        link = link.replace(' ', '_')
-        link = link.replace('.', '_')
-        link = link.replace('-', '_')
-        link = (f'https://www.hdcarwallpapers.com/walls/{link}-HD.jpg')
-        
-        individual_pages.append(link)
+            individual_pages.append(link)
 
 
 def export_to_txt(list_urls):
-    with open(r'/Web_Scarping_Wallpaper/download_list.txt', 'w') as fp:
+    with open(r'download_list.txt', 'a') as fp:
         for link in list_urls:
             # write each item on a new line
             fp.write("%s\n" % link)
 
-     
-url = str(input('Digite a URL: '))
 
-find_page(url)
+first = int(input('Nº da primeira página:  '))
+last = int(input('Nº da última página: '))
+
+create_file(first, last)
 export_to_txt(individual_pages)
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
