a13ff5cd12a0227f19d6dec584882b8bdd3d58b4
Jorge W. Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2025-01-07 20:45:22 -0300

    refactor-function-get_coin (#39)
    Function get_coin improved

--- Currency_Converter/main.py
+++ Currency_Converter/main.py
@@ -14,45 +14,27 @@ def get_exchange_rate(base: str, symbol: str, api_key: str):
     else:
         return f"API access error: {response.status_code}"
 
-def get_base_coin():
-    base = None
-    base_coin = int(input('''SELECT THE BASE COIN
+def get_coin(coin_type):
+    coin_map = {1: 'USD', 2: 'EUR', 3: 'BRL'}
+    coin = None
+
+    while coin is None:
+        try:
+            choice = int(input(f'''SELECT THE {coin_type} COIN
 1 - USD
 2 - EUR
 3 - BRL
 YOUR CHOICE: '''))
 
-    if 1 <= base_coin <= 3:
-        if base_coin == 1:
-            base = 'USD'
-        elif base_coin == 2:
-            base = 'EUR'
-        else:
-            base = 'BRL'
-    else:
-        print('Invalid choice')
+            coin = coin_map.get(choice)
+            if coin is None:
+                print("Invalid choice. Please select a valid option (1, 2, or 3).")
+        except ValueError:
+            print("Invalid input. Please enter a number (1, 2, or 3).")
 
-    return base
+    print('='*40)
+    return coin
 
-def get_symbol_coin():
-    symbol = None
-    symbol_coin = int(input('''SELECT THE SYMBOL COIN
-1 - USD
-2 - EUR
-3 - BRL
-YOUR CHOICE: '''))
-
-    if 1 <= symbol_coin <= 3:
-        if symbol_coin == 1:
-            symbol = 'USD'
-        elif symbol_coin == 2:
-            symbol = 'EUR'
-        else:
-            symbol = 'BRL'
-    else:
-        print('Invalid choice')
-
-    return symbol
 
 api_token = ''
 
@@ -60,7 +42,7 @@ print(f'''{"="*40}
 {"CURRENCY CONVERTER":^40}
 {"="*40}\n''')
 
-selected_base = get_base_coin()
-selected_symbol = get_symbol_coin()
+selected_base = get_coin('BASE')
+selected_symbol = get_coin('SYMBOL')
 exchange_rate = get_exchange_rate(selected_base, selected_symbol, api_token)
 print(f'1 {selected_base} = {exchange_rate} {selected_symbol}')
