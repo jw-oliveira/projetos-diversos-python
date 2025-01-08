91264b289b21803470a7f4aa5363b09a8397a08c
Jorge W. Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2025-01-08 19:46:03 -0300

    Revert "Refactor improve currency converter (#40)"
    This reverts commit 6c67a5c3d1cd4c6a96e2e76002c65056d860a099.

--- Currency_Converter/main.py
+++ Currency_Converter/main.py
@@ -1,21 +1,19 @@
 import requests
 
-# Function to get the exchange rate between two currencies using the ExchangeRate-API
-def get_exchange_rate(base: str, target: str, api_key: str):
+def get_exchange_rate(base: str, symbol: str, api_key: str):
     url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{base}"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        rate = data['conversion_rates'].get(target)
+        rate = data['conversion_rates'].get(symbol)
         if rate:
             return rate
         else:
-            return f'Target {target} not found.'
+            return f'Symbol {symbol} not found.'
     else:
         return f"API access error: {response.status_code}"
 
-# Function to get the user's choice of currency (base or target)
 def get_coin(coin_type):
     coin_map = {1: 'USD', 2: 'EUR', 3: 'BRL'}
     coin = None
@@ -38,14 +36,13 @@ YOUR CHOICE: '''))
     return coin
 
 
-api_token = '83a22e015d3d5aa6521400a9'
+api_token = ''
 
 print(f'''{"="*40}
 {"CURRENCY CONVERTER":^40}
 {"="*40}\n''')
 
 selected_base = get_coin('BASE')
-selected_target = get_coin('TARGET')
-exchange_rate = get_exchange_rate(selected_base, selected_target, api_token)
-# Displays the conversion result
-print(f'1 {selected_base} = {exchange_rate} {selected_target}')
+selected_symbol = get_coin('SYMBOL')
+exchange_rate = get_exchange_rate(selected_base, selected_symbol, api_token)
+print(f'1 {selected_base} = {exchange_rate} {selected_symbol}')
